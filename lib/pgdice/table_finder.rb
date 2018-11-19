9abd8773f6b3f64f901f2a729046a72d40d94a54
Andrew Newell <andrew.newell@genesys.com>
2018-11-19 13:12:50 -0500

    building table_finder with unit tests

--- lib/pgdice/table_finder.rb
+++ lib/pgdice/table_finder.rb
@@ -4,13 +4,30 @@ module PgDice
   # Module which is a collection of methods used by PartitionManager to find and list tables
   class TableFinder
     include PgDice::Loggable
-    extend Forwardable
-
-    def_delegators :@configuration, :batch_size, :approved_tables, :table_dropper
 
     def initialize(configuration = PgDice::Configuration.new)
       @configuration = configuration
     end
+
+
+    def find_droppable_partitions(all_tables, older_than, minimum_tables)
+      tables_older_than = tables_older_than(all_tables, older_than)
+      tables_to_grab = tables_to_grab(tables_older_than.size, minimum_tables)
+      tables_older_than.first(tables_to_grab)
+    end
+
+    def tables_to_grab(eligible_tables_count, minimum_tables)
+      tables_to_grab = eligible_tables_count - minimum_tables
+      tables_to_grab.positive? ? tables_to_grab : 0
+    end
+
+    def tables_older_than(tables, older_than)
+      tables.select do |partition_name|
+        partition_created_at_time = Date.parse(partition_name)
+        partition_created_at_time < older_than.to_date
+      end
+    end
+
 
     def build_partition_table_fetch_sql(params = {})
       schema = params.fetch(:schema, 'public')
--- test/pgdice/table_finder_test.rb
+++ test/pgdice/table_finder_test.rb
@@ -3,4 +3,30 @@
 require 'test_helper'
 
 class TableFinderTest < Minitest::Test
+  def test_find_droppable_partitions
+    results = PgDice::TableFinder.new.find_droppable_partitions(generate_tables, Date.parse('20181022'), 1)
+    assert_equal ["comments_20181020"], results
+  end
+
+  def test_tables_to_grab_returns_0_on_negative_math
+    results = PgDice::TableFinder.new.tables_to_grab(1, 2)
+    assert_equal 0, results
+  end
+
+  def test_find_droppable_tables_returns_empty_list_when_minimum_not_met
+    results = PgDice::TableFinder.new.find_droppable_partitions(generate_tables, Date.parse('20181022'), 2)
+    assert_empty results
+  end
+
+  def test_tables_older_than
+    results = PgDice::TableFinder.new.tables_older_than(generate_tables, Date.parse('20181022'))
+    assert_equal %w(comments_20181020 comments_20181021), results
+  end
+
+
+  private
+
+  def generate_tables
+    %w(comments_20181020 comments_20181021 comments_20181022 comments_20181023 comments_20181024 comments_20181025 comments_20181026)
+  end
 end
