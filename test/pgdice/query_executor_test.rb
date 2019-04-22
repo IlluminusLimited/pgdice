93155e8c522b798d2bb6a82c6f4bd45b3fcc9351
Andrew Newell <andrew.newell@genesys.com>
2019-04-22 14:21:31 -0400

    make methods smaller

--- test/pgdice/query_executor_test.rb
+++ test/pgdice/query_executor_test.rb
@@ -3,27 +3,37 @@
 require 'test_helper'
 
 class QueryExecutorTest < Minitest::Test
+  def setup
+    @should_raise = true
+    @resetter_call_count = 0
+    @runner_call_count = 0
+
+    @resetter = resetter
+    @runner = runner
+  end
+
   def test_retry_on_pg_error
-    should_raise = true
-
-    resetter_call_count = 0
-    resetter = proc do
-      should_raise = false
-      resetter_call_count += 1
-    end
-
-    runner_call_count = 0
-
-    runner = proc do
-      runner_call_count += 1
-      raise PG::Error, 'Something bad' if should_raise
-    end
-
-    PgDice::QueryExecutor.new(logger: logger, connection_supplier: -> { MockPgConnection.new(runner, resetter) })
+    PgDice::QueryExecutor.new(logger: logger, connection_supplier: -> { MockPgConnection.new(@runner, @resetter) })
                          .call('blah')
 
-    assert_equal 2, runner_call_count, 'Runner should be called twice when we catch a PG error'
-    assert_equal 1, resetter_call_count, 'Resetter should be called once when we catch a PG error'
+    assert_equal 2, @runner_call_count, 'Runner should be called twice when we catch a PG error'
+    assert_equal 1, @resetter_call_count, 'Resetter should be called once when we catch a PG error'
+  end
+
+  private
+
+  def resetter
+    proc do
+      @should_raise = false
+      @resetter_call_count += 1
+    end
+  end
+
+  def runner
+    proc do
+      @runner_call_count += 1
+      raise PG::Error, 'Something bad' if @should_raise
+    end
   end
 end
 
