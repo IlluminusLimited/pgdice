01b72a281b0650b2cb10ac0b7ede58ff361f408b
Andrew Newell <andrew.newell@genesys.com>
2022-02-09 10:29:48 -0500

    Remove duplicate tests

--- test/pgdice/table_finder_test.rb
+++ test/pgdice/table_finder_test.rb
@@ -35,39 +35,6 @@ class TableFinderTest < Minitest::Test
     assert_equal generate_tables, results
   end
 
-  def test_safe_date_builder_invalid_date
-    assert_raises(ArgumentError) do
-      safe_date_builder('comments_201')
-    end
-  end
-
-  def test_safe_date_builder_days
-    table_name = 'comments_20181022'
-    assert_equal Date.parse('20181022'), safe_date_builder(table_name)
-  end
-
-  def test_safe_date_builder_months
-    table_name = 'comments_201810'
-    assert_equal Date.parse('20181001'), safe_date_builder(table_name)
-  end
-
-  def test_safe_date_builder_years
-    table_name = 'comments_2018'
-    assert_equal Date.parse('20180101'), safe_date_builder(table_name)
-  end
-
-  def test_truncate_date_day
-    assert_equal Date.parse('20181022'), truncate_date(Date.parse('20181022'), 'day')
-  end
-
-  def test_truncate_date_month
-    assert_equal Date.parse('20181001'), truncate_date(Date.parse('20181022'), 'month')
-  end
-
-  def test_truncate_date_year
-    assert_equal Date.parse('20180101'), truncate_date(Date.parse('20181022'), 'year')
-  end
-
   def test_tables_to_grab_returns_0_on_negative_math
     results = tables_to_grab(1, 2)
     assert_equal 0, results
